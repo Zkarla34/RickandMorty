7faa886b662ec018d9cf86fd3305a4138330745a
Zulma Solano <61100581+Zkarla34@users.noreply.github.com>
2024-10-19 16:57:38 -0500

    Ajustes codigo
    Se ajusto el código para utilizar Sync y mejorar el rendimiento, se ajusto UI

--- Assets/Scripts/APIManager.cs
+++ Assets/Scripts/APIManager.cs
@@ -5,22 +5,30 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using UnityEngine.UI;
 using TMPro;
+using System.Threading.Tasks;
 
 public class APIManager : MonoBehaviour
 {
+    [Header("API Settings")]
     private string apiUrl = "https://rickandmortyapi.com/api/character";
     private int currentPage = 1;
     [SerializeField] private int charactersPerPage;
 
-    public CharacterPool characterPool;
-
-    //Lista para los personajes
-    public List<CharacterAPI> characterInfo = new List<CharacterAPI>();
-
+    [Header("UI Elements")]
     public GameObject characterPanel;
     public GameObject characterPrefab;
     public TextMeshProUGUI pageNumberText;
+    public Slider pageSlider;
+    public TextMeshProUGUI errorMesage;
 
+    [Header("Dependencies")]
+    public CharacterPool characterPool;
+    private Dictionary<string, Sprite> imageCache = new Dictionary<string, Sprite>();
+    public List<CharacterAPI> characterInfo = new List<CharacterAPI>();
+
+
+    //Data Structures
+    [System.Serializable]
     public class CharacterAPI
     {
         public int id;
@@ -44,6 +52,7 @@ public class APIManager : MonoBehaviour
         public List<CharacterAPI> results;
     }
 
+    [System.Serializable]
     public class Info
     {
         public int count;
@@ -52,62 +61,77 @@ public class APIManager : MonoBehaviour
         public string prev;
     }
 
-    private void Start()
+    private async void Start()
     {
         characterPool = GameObject.Find("CharacterPoolManager").GetComponent<CharacterPool>();
-        GetCharacters(currentPage);
-        UpdatePageNunmberUI();
+        if(characterPool == null)
+        {
+            Debug.LogError("No se encontro");
+            return;
+        }
+        await GetCharactersAsync(currentPage);
     }
 
-    public void GetCharacters(int page)
-    {
-        StartCoroutine(GetDataCharacters(page));
-    }
-
-    // Coroutine para obtener personajes de la API
-    private IEnumerator GetDataCharacters(int page)
+    private async Task GetCharactersAsync(int page)
     {
         string url = $"{apiUrl}?page={page}";
-        UnityWebRequest request = UnityWebRequest.Get(url);
-        yield return request.SendWebRequest();
-        if (request.result == UnityWebRequest.Result.ConnectionError
-            || request.result == UnityWebRequest.Result.ProtocolError)
+        using UnityWebRequest request = UnityWebRequest.Get(url);
         {
-            Debug.LogError("Error de la solicitud: " + request.error);
-        }
-        else
-        {
-            string responseData = request.downloadHandler.text;
-            CharacterList list = JsonConvert.DeserializeObject<CharacterList>(responseData);
-            if (list != null && list.results != null)
+            var operation = request.SendWebRequest();
+            while(!operation.isDone)
             {
-                characterInfo.Clear();
-                characterInfo.AddRange(list.results);
+                await Task.Yield();
+            }
 
-                // Añadir los personajes al panel
-                for(int i = 0; i < characterInfo.Count && i < charactersPerPage; i++)
-                {
-                    CharacterAPI character = characterInfo[i];
-                    GameObject newCharacterItem = characterPool.GetCharacter();
-                    newCharacterItem.transform.SetParent(characterPanel.transform, false);
-                    newCharacterItem.transform.Find("NameCharacter").GetComponent<TextMeshProUGUI>().text = character.name;
-                    Debug.Log("personaje: " + character.name);
-
-                    Image characterImage = newCharacterItem.transform.Find("CharacterImage").GetComponent<Image>();
-                    StartCoroutine(LoadImage(character.image, characterImage));
-                }
+            if (request.result == UnityWebRequest.Result.ConnectionError
+                || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+               ShowError("Error de la solicitud: " + request.error);
             }
             else
             {
-                Debug.LogError("La respuesta no contiene datos o esta vacia");
+                string responseData = request.downloadHandler.text;
+                CharacterList list = JsonConvert.DeserializeObject<CharacterList>(responseData);
+
+                if (list != null && list.results != null)
+                {
+                    UpdateUI(list.info.pages); 
+                    UpdateCharacterUI(list.results);
+                    
+                }
+                else
+                {
+                    ShowError("La respuesta no contiene datos o esta vacia");
+                }
             }
+        }  
+    }
 
+    private void UpdateUI(int totalPages)
+    {
+        pageSlider.maxValue = totalPages;
+        pageNumberText.text = $"Page:{currentPage}";
+    }
 
+    private void UpdateCharacterUI(List<CharacterAPI> characters)
+    {
+        characterInfo.Clear();
+        characterInfo.AddRange(characters);
+
+        foreach (CharacterAPI character in characterInfo)
+        {
+            GameObject newCharacterItem = characterPool.GetCharacter();
+            newCharacterItem.transform.SetParent(characterPanel.transform, false);
+            newCharacterItem.transform.Find("NameCharacter").GetComponent<TextMeshProUGUI>().text = character.name;
+            Debug.Log("personaje: " + character.name);
+
+            //Image characterImage = newCharacterItem.transform.Find("CharacterImage").GetComponent<Image>();
+           // LoadImageAsync(character.image, characterImage);
         }
     }
 
-    private Dictionary<string, Sprite> imageCache = new Dictionary<string, Sprite>();
-    private IEnumerator LoadImage(string imageUrl, Image targetImage)
+    /*
+    private async void LoadImageAsync(string imageUrl, Image targetImage)
     {
         if(imageCache.TryGetValue(imageUrl, out Sprite cachedSprite))
         {
@@ -115,53 +139,60 @@ public class APIManager : MonoBehaviour
         }
         else
         {
-            UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageUrl);
-            yield return request.SendWebRequest();
-            if (request.result == UnityWebRequest.Result.ConnectionError
+            using UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageUrl);
+            {
+                var operation = request.SendWebRequest();
+                while(!operation.isDone)
+                {
+                    await Task.Yield();
+                }
+                if (request.result == UnityWebRequest.Result.ConnectionError
                 || request.result == UnityWebRequest.Result.ProtocolError)
-            {
-                Debug.LogError("Error al cargar la imagen: " + request.error);
-            }
-            else
-            {
-                Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
-                Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-                targetImage.sprite = sprite;
-                imageCache[imageUrl] = sprite;
+                {
+                    ShowError("Error al cargar la imagen: " + request.error);
+                }
+                else
+                {
+                    Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+                    Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                    targetImage.sprite = sprite;
+                    imageCache[imageUrl] = sprite;
+                }
             }
         }
     }
-
-    private Dictionary<int, CharacterAPI> characterCache = new Dictionary<int, CharacterAPI>();
-    private void CacheCharacter(CharacterAPI character)
+    */
+    private void ShowError(string message)
     {
-        if(!characterCache.ContainsKey(character.id))
+        Debug.LogError(message);
+        errorMesage.text = message;
+        errorMesage.gameObject.SetActive(true);
+    }
+
+    public async void OnSliderValueChanged()
+    {
+        int pageNumber = Mathf.RoundToInt(pageSlider.value);
+        pageNumberText.text = $"Page: {pageNumber}";
+        currentPage = pageNumber;
+        await GetCharactersAsync(currentPage);
+    }
+    public async void NextPage()
+    {
+        if(currentPage < pageSlider.maxValue)
         {
-            characterCache.Add(character.id, character);
+            currentPage++;
+            pageSlider.value = currentPage;
+            await GetCharactersAsync(currentPage);
         }
     }
 
-
-    public void UpdatePageNunmberUI()
+    public async void PreviousPage()
     {
-        pageNumberText.text = $"Page: {currentPage}";
-    }
-    public void NextPage()
-    {
-        currentPage++;
-        GetCharacters(currentPage);
-        UpdatePageNunmberUI();
-    }
-
-    public void PreviousPage()
-    {
-        if (currentPage > 0)
+        if (currentPage > 1)
         {
             currentPage--;
-            GetCharacters(currentPage);
-            UpdatePageNunmberUI();
+            pageSlider.value = currentPage;
+            await GetCharactersAsync(currentPage);
         }
     }
 }
-
-
--- Assets/Scripts/CharacterPool.cs
+++ Assets/Scripts/CharacterPool.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class CharacterPool : MonoBehaviour
 {
+    public static CharacterPool Instance { get; private set;}
+
     public GameObject characterPrefab;
     public int poolSize = 10;
     private Queue<GameObject> pool = new Queue<GameObject>();
 
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
-        for(int i = 0; i < poolSize; i++)
+        InitializePool();
+    }
+
+    private void InitializePool()
+    {
+        for (int i = 0; i < poolSize; i++)
         {
             GameObject character = Instantiate(characterPrefab);
             character.SetActive(false);
             pool.Enqueue(character);
         }
     }
-
    public GameObject GetCharacter()
-    {
+   {
         GameObject character = pool.Dequeue();
         character.SetActive(true);
         pool.Enqueue(character);
         return character;
+   }
+
+    public void ResetCharacter(GameObject character)
+    {
+        character.SetActive(false);
+        character.transform.SetParent(transform);
+        pool.Enqueue(character);
     }
 }
